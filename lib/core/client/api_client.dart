3e38ffbef2955130f3597bcf6566c8c0e361baad
Lucas <lucasp.devsantna@gmail.com>
2025-09-18 22:48:22 -0300

    fix: Import errado da lib de logs

--- lib/core/client/api_client.dart
+++ lib/core/client/api_client.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+import 'dart:developer';
 
 import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
